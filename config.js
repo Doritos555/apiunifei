dfa6c114b2f3d590c6fdd07cbfddf5635f56d1e8
Doritos555 <joaopedrotavaresdeassis57@gmail.com>
2025-10-30 09:33:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
 const dotenv = require('dotenv');
+
 dotenv.config();
-const {
-    PORT,
-    pgConnection
-} = process.env;
+
 module.exports = {
-    port: PORT,
-    urlConnection: pgConnection
+    PORT: process.env.PORT,
+    DATABASE_URL: process.env.DATABASE_URL
 } 
